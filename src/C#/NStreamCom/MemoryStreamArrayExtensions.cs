1813ef15136afdb39611167f0e41ff0915a0b9b2
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-10-22 20:31:01 -0400

    Added send delay

--- src/C#/NStreamCom/MemoryStreamArrayExtensions.cs
+++ src/C#/NStreamCom/MemoryStreamArrayExtensions.cs
@@ -1,13 +1,17 @@
-﻿using System.IO;
+﻿using System.Threading;
+using System.IO;
 
 namespace NStreamCom
 {
     public static class MemoryStreamArrayExtensions
     {
-        public static void WriteStreamsTo(this Stream[] Source, Stream Destination)
+        public static void WriteStreamsTo(this Stream[] Source, Stream Destination, int WaitTime = 3)
         {
             foreach (var Stream in Source)
+            {
                 Destination.Write(Stream);
+                Thread.Sleep(WaitTime);
+            }
         }
     }
 }
